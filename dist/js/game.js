864fce401b55bb4e85643061e9864b1f76d91f8b
Marc Henklein <snooze82@gmail.com>
2014-04-26 17:57:12 +0200

    added instructions

--- dist/js/game.js
+++ dist/js/game.js
@@ -385,9 +385,13 @@ Menu.prototype = {
   create: function() {
     var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, 'FearWire', style);
+    this.titleText = this.game.add.text(this.game.world.centerX, 120, 'FearWire', style);
     this.titleText.anchor.setTo(0.5, 0.5);
-
+    this.game.add.text(170,200,'* send an impulse by clicking on the head (left) (lvl2: top)', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.game.add.text(170,220,'* toggle switches (should show the direction later)', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.game.add.text(170,240,'* kill all flowers', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.game.add.text(170,260,'* (lvl2) you can toggle the switch while the impulse is traveling', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.game.add.text(60,150,'story: you are a doctor who can destroy somebodies fears by rewiring his/hers brain (inception like)',  { font: '16px Arial', fill: '#ffffff', align: 'center'})
     this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play', { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionsText.anchor.setTo(0.5, 0.5);
 
